ecca7585bce389d30f2184cfb98c85729fe82a02
h_farjam <hosseinfarjamfar@gmail.com>
2025-09-15 21:31:42 +0330

    finally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl text-center my-10">Contact</h1>
+<div className="flex flex-col justify-center items-center">
+        <h1 className="text-3xl text-center my-10">Contact</h1>
       <p>please Login and Create your first Contact</p>
       <Link className="border px-4 py-1 rounded-2xl my-10" href={'/auth/register'}>register</Link>
+</div>
     </>
   );
 }
