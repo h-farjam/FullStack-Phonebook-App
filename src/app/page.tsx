8a0e14f10b3034dda3df367fda1c8624b7c8492f
h_farjam <hosseinfarjamfar@gmail.com>
2025-09-16 18:40:53 +0330

    Edit Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,20 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-<div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
         <h1 className="text-3xl text-center my-10">Contact</h1>
-      <p>please Login and Create your first Contact</p>
-      <Link className="border px-4 py-1 rounded-2xl my-10" href={'/auth/register'}>register</Link>
-</div>
+        <p>please Login and Create your first Contact</p>
+        <Link
+          className="border px-4 py-1 rounded-2xl my-10"
+          href={"/auth/register"}
+        >
+          register
+        </Link>
+      </div>
+      <footer className="w-full text-center py-4 border-t border-gray-200 text-gray-500 text-sm">
+        Full-Stack project built with Next.js, TypeScript, Tailwind CSS, Next.js
+        API Routes, and MongoDB
+      </footer>
     </>
   );
 }
